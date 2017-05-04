61ce275d8e704af385ff57efd7ae09381f8a6b95
JohnG585 <jgallagher585@gmail.com>
2017-05-04 10:03:15 -0600

    sort posts

--- reddit-clone-part-1/app.module.js
+++ reddit-clone-part-1/app.module.js
@@ -20,7 +20,38 @@
 
         vm.$onInit = function() {
           vm.posts = [
-            {title: 'Hey now', author:"Dale", body:"Testing", image:"./images/pexels-photo-69212.jpeg", votes:4}
+            {
+            id: 1,
+            title: 'Found the best recipe for flan!',
+            author:"Dayle",
+            body:"If anyone wants it, please send up a smoke signal. I am against technology and recognizing irony.",
+            image:"./images/pexels-photo-69212.jpeg",
+            votes:4
+            },
+            {
+            id:2,
+            title: 'Father John Misty Stole My Mustache',
+            author: 'Swain',
+            body: "That Leonard Cohen-knockoff can\'t even grow original facial hair. Unfollow.",
+            image:"./images/pexels-photo-121442.jpeg",
+            votes: 2
+            },
+            {
+            id:3,
+            title: 'I have nothing to say',
+            author: 'Tiphany',
+            body: "What\'s the point...ya know?",
+            image:"./images/pexels-photo-192440.jpeg",
+            votes: 25
+            },
+            {
+            id:4,
+            title: 'Has Sylvan Esso Sold Out??',
+            author: 'Heathcliff',
+            body: "I was a HUGE fan of theirs back when Amelia had just left Mountain Man, but ever since 'Radio' became a huge hit both of them have stopped replying to my emails. Like, what's the point of becoming famous if you're just going to ignore the fans who have been there since the beginning, you know?",
+            image:"./images/pexels-photo-214135.jpeg",
+            votes: 6
+            },
           ]
         }
         vm.postAdded = function() {
@@ -40,7 +71,9 @@
               post.votes--
             }
         }
-
+        vm.filtering = function(filter) {
+            vm.selection = filter
+        }
       }
 
       function CommentController() {
